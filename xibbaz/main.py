f12ecf738e9e62defbc911bb5e8269a1b53db016
Erik Stephens <estephens@terascope.io>
2018-05-23 08:33:16 -0700

    Usage/help for main cmd wrapper

--- xibbaz/main.py
+++ xibbaz/main.py
@@ -1,8 +1,22 @@
 """
-A stupid wrapper around scripts to facilitate a single Dockerfile ENTRYPOINT.
+A wrapper around scripts to facilitate a single Dockerfile ENTRYPOINT.
+
+  usage: xibbaz.main <cmd> ...
+
+Where <cmd> is one of the following.  Use `-h, --help` for cmd specific usage.
+  - cli
+  - group
+  - template
+  - triggers
 """
 
 import sys
 import importlib
 
-importlib.import_module('xibbaz.cmd.' + sys.argv[1]).main(sys.argv[2:])
+if len(sys.argv) >= 2:
+    if sys.argv[1] not in ('cli', 'group', 'template', 'triggers'):
+        print(__doc__)
+    else:
+        importlib.import_module('xibbaz.cmd.' + sys.argv[1]).main(sys.argv[2:])
+else:
+    print(__doc__)
